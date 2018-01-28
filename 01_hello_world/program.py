1c8db58ce2fbcc61577e8d28ee1fc3287ae891e4
LycheeXD <mchen7588@gmail.com>
2018-01-28 14:12:36 -0800

    hello world

---
+++ 01_hello_world/program.py
@@ -0,0 +1,10 @@
+print('-------------------')
+print('       HELLO')
+print('-------------------')
+print()
+
+user_name = input('whats your name? ')
+
+greetings = 'hello ' + user_name
+
+print(greetings)
